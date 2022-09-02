84266ed4e4ae796d3214720ab64c962869273649
patrickfv <patrickfv.br@gmail.com>
2022-09-02 16:08:53 -0300

    refactor: about navigation

--- app/src/main/java/com/patrickfv/lodjinha/ui/about/AboutFragment.kt
+++ app/src/main/java/com/patrickfv/lodjinha/ui/about/AboutFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.patrickfv.lodjinha.databinding.FragmentGalleryBinding
+import com.patrickfv.lodjinha.databinding.FragmentAboutBinding
 
-class GalleryFragment : Fragment() {
+class AboutFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentAboutBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-                ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val aboutViewModel =
+                ViewModelProvider(this).get(AboutViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textAbout
+        aboutViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
